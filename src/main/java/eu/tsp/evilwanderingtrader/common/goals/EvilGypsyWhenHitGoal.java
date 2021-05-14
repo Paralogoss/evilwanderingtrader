3d5621f623ba1ed9ed5a24ad7f4e6a8891d437ef
victor.laforet <victor.laforet@telecom-sudparis.eu>
2021-05-14 15:33:47 +0200

    Ajout GypsyWanderingTrader et transformation lorsqu'on lui donne un coup en Gypsy

---
+++ src/main/java/eu/tsp/evilwanderingtrader/common/goals/EvilGypsyWhenHitGoal.java
@@ -0,0 +1,29 @@
+package eu.tsp.evilwanderingtrader.common.goals;
+
+import eu.tsp.evilwanderingtrader.EvilWanderingTrader;
+import eu.tsp.evilwanderingtrader.common.entities.GypsyWanderingTraderEntity;
+import eu.tsp.evilwanderingtrader.init.ModEntityTypes;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.player.PlayerEntity;
+
+public class EvilGypsyWhenHitGoal extends Goal {
+	protected final GypsyWanderingTraderEntity wanderingTrader;
+
+	public EvilGypsyWhenHitGoal(GypsyWanderingTraderEntity wanderingTrader) {
+		this.wanderingTrader = wanderingTrader;
+	}
+
+	@Override
+	public boolean shouldExecute() {
+		return this.wanderingTrader.getRevengeTarget() != null && this.wanderingTrader.getRevengeTarget() instanceof PlayerEntity;
+	}
+
+	@Override
+	public void startExecuting() {
+		if (net.minecraftforge.event.ForgeEventFactory.canLivingConvert(this.wanderingTrader, ModEntityTypes.GYPSY.get(), (timer) -> {})) {
+			EvilWanderingTrader.LOGGER.info("Converting");
+			PlayerEntity player = (PlayerEntity) this.wanderingTrader.getRevengeTarget();
+			wanderingTrader.turnIntoGypsy(player);
+		}
+	}
+}
